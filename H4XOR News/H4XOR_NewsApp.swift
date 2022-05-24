9667743bf0ee104af4c5a48256aae6e3f48d9f66
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 14:35:37 -0300

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Lilian De Oliveira Silva on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
